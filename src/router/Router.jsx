bbd112cb48673bb9a411852ae56e79ce7c4f798a
MatthieuDEP <matthieu.depret.md@gmail.com>
2024-02-16 11:59:52 +0100

    Création d'un composant pour le router

---
+++ src/router/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/home/Home';
+import About from '../pages/about/About';
+import Housing from '../pages/housing/Housing';
+import NotFound from '../pages/notFound/NotFound';
+
+export default function Router() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/housing" element={<Housing />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
+};
