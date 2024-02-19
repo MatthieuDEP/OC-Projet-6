4c37ed266a0c192162028a085bcbafd74c471e38
MatthieuDEP <matthieu.depret.md@gmail.com>
2024-02-19 17:30:54 +0100

    Page 404

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './notFound.scss';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
-        <div className="notFound">
-            <p>404</p>
-        </div>
+        <main className="notFound">
+            <p className="notFoundNumber">404</p>
+            <p className="notFoundText">Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="notFoundLink">Retourner sur la page d'accueil</Link>
+        </main>
     );
 };
